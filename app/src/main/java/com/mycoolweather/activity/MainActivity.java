a39a1e98fbf0618ea99ddff0e91f104ce6e6468f
yuanhenghui <974609673@qq.com>
2017-02-15 16:10:46 +0800

    update MainActivity

--- app/src/main/java/com/mycoolweather/activity/MainActivity.java
+++ app/src/main/java/com/mycoolweather/activity/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //缓存内容
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getString("weather", null) != null) {
             Intent intent = new Intent(this, WeatherActivity.class);
